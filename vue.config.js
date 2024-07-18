ea72d0e48d4e7622d47baf9b24740ed990d38c37
nam <nam@aa.com>
2024-07-18 09:40:30 +0900

    커밋

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/study_vueJs/'  // GitHub Pages에 호스팅될 경로 설정
-    : '/',
-  outputDir: 'dist'  // 빌드된 파일들이 생성될 폴더 설정
+    : '/'
 }
