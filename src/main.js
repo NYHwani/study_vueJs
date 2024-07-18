d43654aa8b8ca52127d861e827ce0e8a7f618843
nam <nam@aa.com>
2024-07-18 10:29:49 +0900

    커밋

--- src/main.js
+++ src/main.js
@@ -5,16 +5,28 @@ import mixins from './mixins'
 //import router2 from './router/index1.js'
 
 
-createApp(App)
-.use(router)
-.mixin(mixins)
-.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mixin(mixins);
 
+// Vue 애플리케이션이 마운트된 후에 라우터를 사용하여 라우트 이동
+router.isReady().then(() => {
+  app.mount('#app');
 
-/*createApp(App).use(router2).mount('#test')
-router2.push({
-    path: 'databinding/',
-    params: {
-        userNo: 1
+  // Vue Router Navigation Guard를 사용하여 404 에러 발생 시 홈페이지로 리디렉션
+  router.beforeEach((to, from, next) => {
+    if (to.matched.length === 0) { // 매칭되는 라우트가 없는 경우
+      next({ name: 'Home' }); // 홈페이지로 리디렉션
+    } else {
+      next(); // 다음 라우트로 이동
     }
-})*/
+  });
+
+  // 사용자 상호작용이나 특정 이벤트 발생 시 라우팅을 시도하는 경우
+  // router.push({
+  //   name: 'DatabindingDetail',
+  //   params: {
+  //     userNo: 1 // 예시로 1을 넘김
+  //   }
+  // });
+});
