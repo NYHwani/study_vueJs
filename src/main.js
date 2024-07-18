74867ef0e5d468763419e0cedd7897bb43468119
nam <nam@aa.com>
2024-07-18 10:32:16 +0900

    커밋

--- src/main.js
+++ src/main.js
@@ -9,24 +9,26 @@ const app = createApp(App);
 app.use(router);
 app.mixin(mixins);
 
-// Vue 애플리케이션이 마운트된 후에 라우터를 사용하여 라우트 이동
+// Vue Router가 초기화된 후에 애플리케이션을 마운트
 router.isReady().then(() => {
-  app.mount('#app');
+    // Vue Router Navigation Guard 설정
+    router.beforeEach((to, from, next) => {
+        if (to.matched.length === 0) {
+        // 매칭되는 라우트가 없으면 홈페이지로 리디렉션
+        next({ name: 'Home' });
+        } else {
+        next(); // 다음 라우트로 이동
+        }
+    });
 
-  // Vue Router Navigation Guard를 사용하여 404 에러 발생 시 홈페이지로 리디렉션
-  router.beforeEach((to, from, next) => {
-    if (to.matched.length === 0) { // 매칭되는 라우트가 없는 경우
-      next({ name: 'Home' }); // 홈페이지로 리디렉션
-    } else {
-      next(); // 다음 라우트로 이동
-    }
-  });
+    // 사용자 상호작용이나 특정 이벤트 발생 시 라우팅을 시도
+    router.push({
+        name: 'DatabindingDetail',
+        params: {
+        userNo: 1 // 예시로 1을 넘김
+        }
+    });
 
-  // 사용자 상호작용이나 특정 이벤트 발생 시 라우팅을 시도하는 경우
-  // router.push({
-  //   name: 'DatabindingDetail',
-  //   params: {
-  //     userNo: 1 // 예시로 1을 넘김
-  //   }
-  // });
+    // 라우터 초기화 후 애플리케이션 마운트
+    app.mount('#app');
 });
